d4d5a091fde3b838390dc44218b86f8abc43ae41
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-23 04:44:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,25 +16,3 @@ class BoardMember {
 
 
 
-    class User {
-      constructor(name, email) {
-        this.name = name;
-        this.email = email;
-      }
-     
-      sayHello() {
-        console.log(`Hello, my name is ${this.name}`);
-      }
-    }
-     
-     
-    class Teacher extends User {
-      sayHello(){
-        super.sayHello()
-        console.log('hello')
-      }
-    }
-     
-    tom.sayHello()
-    // Hello, my name is tom
-    // hello
